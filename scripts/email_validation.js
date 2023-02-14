b592992f16a7f1e17c114be87a4b5e1d7fd63a80
Szymon Paprocki <s.paprocki.vs@gmail.com>
2023-02-14 19:04:43 +0100

    Added mail verification and finished mobile form styles

---
+++ scripts/email_validation.js
@@ -0,0 +1,56 @@
+const emailInputElement = document.getElementById('email-address');
+const submitButtonElement = document.getElementById('submit-button');
+const errorMessageElement = document.getElementById('error-message');
+
+submitButtonElement.addEventListener('click', evaluateEmail);
+
+function evaluateEmail(event) {
+  event.preventDefault();
+  if (!emailIsValid(emailInputElement)) {
+    if (emailInputElement.value == '') {
+      showErrorEmpty();
+    } else {
+      showErrorWrong();
+    }
+    moveSubmitButtonDown();
+  } else {
+    clearError();
+    moveSubmitButtonUp();
+  }
+}
+
+function emailIsValid(emailInput) {
+  const emailInputText = emailInput.value;
+  const emailRegex = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+
+  if (emailInputText.match(emailRegex)) {
+    return true;
+  } else return false;
+}
+
+function showErrorEmpty() {
+  errorMessageElement.classList.add('visible');
+  errorMessageElement.textContent = 'Enter your email please';
+  emailInputElement.classList.add('error');
+}
+
+function showErrorWrong() {
+  errorMessageElement.classList.add('visible');
+  errorMessageElement.textContent = 'Check your email please';
+  emailInputElement.classList.add('error');
+}
+
+function clearError() {
+  errorMessageElement.classList.remove('visible');
+  emailInputElement.classList.remove('error');
+}
+
+function moveSubmitButtonDown() {
+  submitButtonElement.style.top =
+    'calc(2rem + clamp(3.125rem, 1.5vw + 2.77rem, 4.125rem))';
+}
+
+function moveSubmitButtonUp() {
+  submitButtonElement.style.top =
+    'calc(clamp(3.125rem, 1.5vw + 2.77rem, 4.125rem))';
+}
